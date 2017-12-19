018171c007eb22b58c8fe0fe18ea1729fc9778b8
FYX <1449038397@qq.com>
2017-12-19 19:58:19 +0800

    这是第一次上传

---
+++ multiple_teachers/aggregation.py
@@ -0,0 +1,136 @@
+# coding: utf-8
+# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+
+
+def labels_from_probs(probs):
+  """
+  Helper function: computes argmax along last dimension of array to obtain
+  labels (max prob or max logit value) #Helper函数：根据数组的最后一维计算argmax来获得标签（最大概率或最大logit值）
+  :param probs: numpy array where probabilities or logits are on last dimension  #numpy数组，其中概率或logits在最后维度上
+  :return: array with same shape as input besides last dimension with shape 1
+          now containing the labels#数组的形状与输入相同，除了形状1的最后一个维数现在包含标签
+  """
+  # Compute last axis index
+  last_axis = len(np.shape(probs)) - 1
+
+  # Label is argmax over last dimension标签是最后一个维度的argmax
+  labels = np.argmax(probs, axis=last_axis)#numpy.argmax返回沿轴的最大值的索引。
+
+  # Return as np.int32
+  return np.asarray(labels, dtype=np.int32)#numpy.asarray将输入转换为数组。
+
+#这种聚合机制采用了对相同输入进行推理得到的几个模型的softmax / logit输出，并计算候选类的
+# 投票的最高 - 噪声 - 最大值，为每个样本选择一个标签：它将拉普拉斯噪声添加到标签计数中，并返回最频繁 标签
+def noisy_max(logits, lap_scale, return_clean_votes=False):
+  """
+  This aggregation mechanism takes the softmax/logit output of several models
+  resulting from inference on identical inputs and computes the noisy-max of
+  the votes for candidate classes to select a label for each sample: it
+  adds Laplacian noise to label counts and returns the most frequent label.
+  :param logits: logits or probabilities for each sample #每个样本的逻辑或概率
+  :param lap_scale: scale of the Laplacian noise to be added to counts#拉普拉斯噪声的规模将被添加到计数
+  :param return_clean_votes: if set to True, also returns clean votes (without
+                      Laplacian noise). This can be used to perform the
+                      privacy analysis of this aggregation mechanism.#如果设置为True，也将返回干净的选票（没有拉普拉斯噪音）。 这可以用来执行这个聚合机制的隐私分析。
+  :return: pair of result and (if clean_votes is set to True) the clean counts
+           for each class per sample and the the original labels produced by
+           the teachers.#结果对（如果clean_votes设置为True），每个样本的每个类的干净计数和教师产生的原始标签
+  """
+
+  # Compute labels from logits/probs and reshape array properly#从logits / probs计算标签并正确地重新整形数组
+  labels = labels_from_probs(logits)
+  labels_shape = np.shape(labels)
+  labels = labels.reshape((labels_shape[0], labels_shape[1]))
+
+  # Initialize array to hold final labels初始化数组以保存最终的标签
+  result = np.zeros(int(labels_shape[1]))#numpy.zeros返回给定形状和类型的新数组，用零填充。
+
+  if return_clean_votes:#初始化数组，为每个示例保持干净的选票
+    # Initialize array to hold clean votes for each sample
+    clean_votes = np.zeros((int(labels_shape[1]), 10))
+
+  # Parse each sample解析每个样本
+  for i in xrange(int(labels_shape[1])):
+    # Count number of votes assigned to each class 计算分配给每个类的投票数
+    label_counts = np.bincount(labels[:, i], minlength=10)#np.bincount()：计算非负整数数组中每个值的出现次数。
+
+    if return_clean_votes:
+      # Store vote counts for export存储投票计数出口
+      clean_votes[i] = label_counts
+
+    # Cast in float32 to prepare before addition of Laplacian noise在加入拉普拉斯噪声之前，先用float32进行准备
+    label_counts = np.asarray(label_counts, dtype=np.float32)#numpy.asarray将输入转换为数组。
+
+    # Sample independent Laplacian noise for each class每个类都有独立的Laplacian噪音
+    for item in xrange(10):
+      label_counts[item] += np.random.laplace(loc=0.0, scale=float(lap_scale))
+      #numpy.random.laplace从拉普拉斯或指定位置（或平均值）和比例（衰减）的双指数分布绘制样本。
+
+    # Result is the most frequent label结果是最频繁的标签
+    result[i] = np.argmax(label_counts)#返回沿轴的最大值的索引
+
+  # Cast labels to np.int32 for compatibility with deep_cnn.py feed dictionaries
+  #为了与deep_cnn.py字典兼容，将标签转换为np.int32
+  result = np.asarray(result, dtype=np.int32)
+
+  if return_clean_votes:
+    # Returns several array, which are later saved:
+    # result: labels obtained from the noisy aggregation
+    # clean_votes: the number of teacher votes assigned to each sample and class
+    # labels: the labels assigned by teachers (before the noisy aggregation)
+    return result, clean_votes, labels
+  else:
+    # Only return labels resulting from noisy aggregation
+    return result
+
+#这种聚合机制采用了几个模型的softmax / logit输出，这些模型是通过对相同输入进行推理得出的，
+# 并计算出最频繁的标签。 这是确定性的（没有像上面的noisy_max（）那样的噪声注入
+def aggregation_most_frequent(logits):
+  """
+  This aggregation mechanism takes the softmax/logit output of several models
+  resulting from inference on identical inputs and computes the most frequent
+  label. It is deterministic (no noise injection like noisy_max() above.
+  :param logits: logits or probabilities for each sample
+  :return:
+  """
+  # Compute labels from logits/probs and reshape array properly从logits / probs计算标签并正确地重新整形数组
+  labels = labels_from_probs(logits)
+  labels_shape = np.shape(labels)
+  labels = labels.reshape((labels_shape[0], labels_shape[1]))
+
+  # Initialize array to hold final labels
+  result = np.zeros(int(labels_shape[1]))
+
+  # Parse each sample
+  for i in xrange(int(labels_shape[1])):
+    # Count number of votes assigned to each class
+    label_counts = np.bincount(labels[:, i], minlength=10)#np.bincount()：计算非负整数数组中每个值的出现次数。
+
+    label_counts = np.asarray(label_counts, dtype=np.int32)
+
+    # Result is the most frequent label
+    result[i] = np.argmax(label_counts)
+
+  return np.asarray(result, dtype=np.int32)
+
+
---
+++ multiple_teachers/train_teachers.py
@@ -0,0 +1,106 @@
+# coding=utf-8
+# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+
+from differential_privacy.multiple_teachers import deep_cnn
+from differential_privacy.multiple_teachers import input
+from differential_privacy.multiple_teachers import metrics
+
+
+tf.flags.DEFINE_string('dataset', 'svhn', 'The name of the dataset to use')
+tf.flags.DEFINE_integer('nb_labels', 10, 'Number of output classes')
+
+tf.flags.DEFINE_string('data_dir','/tmp','Temporary storage')
+tf.flags.DEFINE_string('train_dir','/tmp/train_dir',
+                       'Where model ckpt are saved')
+
+tf.flags.DEFINE_integer('max_steps', 3000, 'Number of training steps to run.')
+tf.flags.DEFINE_integer('nb_teachers', 50, 'Teachers in the ensemble.')
+tf.flags.DEFINE_integer('teacher_id', 0, 'ID of teacher being trained.')
+
+tf.flags.DEFINE_boolean('deeper', False, 'Activate deeper CNN model')
+
+FLAGS = tf.flags.FLAGS
+
+# 软件名英文小写+特殊手机+特殊支付 eg:qq15180864978@ wx15180864978@
+
+def train_teacher(dataset, nb_teachers, teacher_id):
+  """
+  This function trains a teacher (teacher id) among an ensemble of nb_teachers
+  models for the dataset specified.
+  :param dataset: string corresponding to dataset (svhn, cifar10)
+  :param nb_teachers: total number of teachers in the ensemble
+  :param teacher_id: id of the teacher being trained
+  :return: True if everything went well
+  """
+  # If working directories do not exist, create them
+  assert input.create_dir_if_needed(FLAGS.data_dir)
+  assert input.create_dir_if_needed(FLAGS.train_dir)
+
+  # Load the dataset
+  if dataset == 'svhn':
+    train_data,train_labels,test_data,test_labels = input.ld_svhn(extended=True)
+  elif dataset == 'cifar10':
+    train_data, train_labels, test_data, test_labels = input.ld_cifar10()
+  elif dataset == 'mnist':
+    train_data, train_labels, test_data, test_labels = input.ld_mnist()
+  else:
+    print("Check value of dataset flag")
+    return False
+
+  # Retrieve subset of data for this teacher检索每个教师的数据子集
+  data, labels = input.partition_dataset(train_data,
+                                         train_labels,
+                                         nb_teachers,
+                                         teacher_id)
+
+  print("Length of training data: " + str(len(labels)))
+
+  # Define teacher checkpoint filename and full path定义教师检查点文件名和完整路径
+  if FLAGS.deeper:
+    filename = str(nb_teachers) + '_teachers_' + str(teacher_id) + '_deep.ckpt'
+  else:
+    filename = str(nb_teachers) + '_teachers_' + str(teacher_id) + '.ckpt'
+  ckpt_path = FLAGS.train_dir + '/' + str(dataset) + '_' + filename
+
+  # Perform teacher training 进行教师培训
+  assert deep_cnn.train(data, labels, ckpt_path)
+
+  # Append final step value to checkpoint for evaluation 将最后的步骤值附加到检查点以进行评估
+  ckpt_path_final = ckpt_path + '-' + str(FLAGS.max_steps - 1)
+
+  # Retrieve teacher probability estimates on the test data 检索测试数据的教师概率估计
+  teacher_preds = deep_cnn.softmax_preds(test_data, ckpt_path_final)
+
+  # Compute teacher accuracy
+  precision = metrics.accuracy(teacher_preds, test_labels)
+  print('Precision of teacher after training: ' + str(precision))
+
+  return True
+
+
+def main(argv=None):  # pylint: disable=unused-argument
+  # Make a call to train_teachers with values specified in flags
+  assert train_teacher(FLAGS.dataset, FLAGS.nb_teachers, FLAGS.teacher_id)
+
+if __name__ == '__main__':
+  tf.app.run()
